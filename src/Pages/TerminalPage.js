35255aaf4a99279f07038cc8d4ff14ecaff5812a
ADEEP759 <ADEEP7595@GMAIL.COM>
2023-02-05 00:06:28 +0530

    Today's task is finished.

--- src/Pages/TerminalPage.js
+++ src/Pages/TerminalPage.js
@@ -17,55 +17,86 @@ import { AB_TerminalSchema } from '../Validations/AbTerminal';
 
 
 const TerminalPage = () => {
- 
-
-
   // const [terminalData,setterminalData]=useState();
- 
 // const [terminalData,setterminalData]=useState();
   const { values,errors,touched,handleBlur,handleChange,handleSubmit, } = useFormik({
     initialValues,
     validationSchema:AB_TerminalSchema,
    
     onSubmit:(async (values) => {
-      await new Promise((r) => setTimeout(r, 500));
+      await new Promise((r) => setTimeout(r, 10));
       
      console.log(values);
      
     })
-    
-    
   })
-  // const handleTextChange=(e) =>{
-  //   if (e.target.value.length <= maxLength) {
-  //     this.setState({ value: e.target.value });
-  //   }
-  //   if (e.target.value.length === maxLength) {
-  //     React.findDOMNode(this.nextComponent).focus();
-  //   }
-  // }
-    // console.log(terminalData)
+
+  
+
+  const ChangeTab=()=>{
+    let val1=document.getElementById("formIndex1").value;
+    
+    let b=document.getElementById("formIndex2")
+   
+    if(val1.length>11){
+      return b.focus()
+    }
+  }
+
+  const ChangeTab2=()=>{
+    let val2=document.getElementById("formIndex2").value;
+    let c=document.getElementById("formIndex3")
+    if(val2.length>3){
+    return c.focus()
+  }  }
+
+  const ChangeTab3=()=>{
+    let val3=document.getElementById("formIndex3").value;
+    let d=document.getElementById("formIndex4")
+    if(val3.length>13){
+    return d.focus()
+  }  }
+
+  window.addEventListener('load', function()
+   {
+      document.getElementById("formIndex1").focus();
+  })
+
+  const ShowImage=()=>{
+    document.getElementById("Image_TerminalPage").style.backgroundImage = "url(../../Images/OK.gif)";
+    document.getElementById("Image_TerminalPage").style.backgroundRepeat = "no-repeat";
+    document.getElementById("Image_TerminalPage").style.backgroundSize = "cover";
+    // elmes.style.backgroundImage="url(../../Images/OK.gif)";
+      console.log("ShowImage is Working")
+  }
+
+ 
+
+  
   return (
-      <div> 
+      <div > 
        <Header/>
         <div >
           <form id='Form_Terminal_Page'
+         
           onSubmit={handleSubmit}>
            <div className="mb-1">
              <label htmlFor="exampleInputEmail1" className="form-label">Frame No.</label>
-             <input maxLength="25" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+             <input maxLength="25" className="form-control" id="formIndex1" aria-describedby="emailHelp"
              name='Frame_No'
              autoComplete='off'
+             onKeyUp={(e)=>{ChangeTab(e)}}
              value={values.Frame_No}
-             onChange={handleChange}
+             onChange={(e)=>{handleChange(e)}}
              onBlur={handleBlur}></input>
              {errors.Frame_No && touched.Frame_No?(<p className='form-error'>{errors.Frame_No}</p>):null}
            </div>
            <div className="mb-1">
              <label htmlFor="exampleInputEmail1" className="form-label">Country Code</label>
-             <input maxLength="4" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+             <input maxLength="4" className="form-control" id="formIndex2" aria-describedby="emailHelp"
                name='Country_Code'
                autoComplete='off'
+               onKeyUp={(e)=>{ChangeTab2(e)}}
                value={values.Country_Code}
                onChange={handleChange}                   
                onBlur={handleBlur}></input>
@@ -73,8 +104,9 @@ const TerminalPage = () => {
            </div>
            <div className="mb-1">
              <label htmlFor="exampleInputEmail1" className="form-label">Engine No. </label>
-             <input maxLength="30" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+             <input maxLength="30" className="form-control" id="formIndex3" aria-describedby="emailHelp"
                name='Engine_No'
+               onKeyUp={(e)=>{ChangeTab3(e)}}
                autoComplete='off'
                value={values.Engine_No}
                onChange={handleChange}
@@ -83,7 +115,7 @@ const TerminalPage = () => {
            </div>
            <div className="mb-2">
              <label htmlFor="exampleInputPassword1" className="form-label">Remark(O/R/S)</label>
-             <input maxLength="1" className="form-control" id="exampleInputPassword1"
+             <input maxLength="1" className="form-control" id="formIndex4"
               name='Remarks'
               autoComplete='off'
               value={values.Remarks}
@@ -91,7 +123,7 @@ const TerminalPage = () => {
               onBlur={handleBlur}></input>
                {errors.Remarks && touched.Remarks?(<p className='form-error'>{errors.Remarks}</p>):null}
            </div>
-           <button className="SubmitBTN_Terminal" type='submit' value="AddData" onClick={handleSubmit} >Submit</button>
+           <button className="SubmitBTN_Terminal" type='submit' value="AddData" onClick={(e)=>{handleSubmit(e);ShowImage()}} >Submit</button>
           </form>
         </div>
         <div id="Image_TerminalPage">
